94c35ac32b18e7e140fb6c7c3d0e3cfc619e13d4
Pavel Tarnopolsky <pavel.tarnopolsky@soundcloud.com>
2020-06-14 16:57:52 +0200

    bidirectional jira2md works on both textareas

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jira Markdown
+
+## Up next
+- [V] Add markdown button
+- [ ] Make buttons play nicely with original ones
+- [ ] Add markdown editor
+- [ ] Add draft saving
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
+import j2m from "jira2md"
+
 let isInjected = false;
 
 const injectControl = () => {
   const editField = document.getElementById('description-wiki-edit');
-
   const buttonBar = editField.getElementsByClassName('aui-nav')[0];
+  const wikiTextArea = document.getElementById("description") as HTMLTextAreaElement
 
+  // Create text area
+  const mdTextArea = document.createElement("textarea")
+
+  mdTextArea.addEventListener("input", (e) => {
+    const target = e.target as HTMLTextAreaElement
+    wikiTextArea.value = j2m.to_jira(target.value)
+  })
+
+  wikiTextArea.addEventListener("input", (e) => {
+    const target = e.target as HTMLTextAreaElement
+    mdTextArea.value = j2m.to_markdown(target.value)
+  })
+
+  // Create button
   const mdButtonItem = document.createElement('li');
-  mdButtonItem.innerHTML = `<a href="#">Markdown</a>`
+
+  mdButtonItem.innerHTML = `<a href="#">Markdown</a>`;
+
+  mdButtonItem.addEventListener('click', () => {
+    buttonBar
+      .getElementsByClassName('aui-nav-selected')[0]
+      .classList.remove('aui-nav-selected');
+    mdButtonItem.classList.add('aui-nav-selected');
+  });
+
+  editField.prepend(mdTextArea)
   buttonBar.appendChild(mdButtonItem);
   return true;
 };
