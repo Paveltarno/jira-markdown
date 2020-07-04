a8c260393d6c7a6be339371ecdcc8aca9b1ecd0f
Pavel Tarnopolsky <pavel.tarnopolsky@soundcloud.com>
2020-07-04 18:47:47 +0200

    switch to md toggle control

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## Up next
 - [V] Add markdown button
 - [ ] Make buttons play nicely with original ones
+- [ ] Visual editor to MD
 - [ ] Add markdown editor
 - [ ] Add draft saving
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,77 @@
-import j2m from "jira2md"
+import j2m from 'jira2md';
 
 let isInjected = false;
 
-const injectControl = () => {
-  const editField = document.getElementById('description-wiki-edit');
-  const buttonBar = editField.getElementsByClassName('aui-nav')[0];
-  const wikiTextArea = document.getElementById("description") as HTMLTextAreaElement
+const toggleVisibility = (elem: HTMLElement) => {
+  elem.style.display = elem.style.display === 'none' ? 'block' : 'none';
+};
 
-  // Create text area
-  const mdTextArea = document.createElement("textarea")
+const createControlBar = (
+  wikiTextArea: HTMLTextAreaElement,
+  mdTextArea: HTMLTextAreaElement
+) => {
+  const mdControlBar = document.createElement('div');
 
-  mdTextArea.addEventListener("input", (e) => {
-    const target = e.target as HTMLTextAreaElement
-    wikiTextArea.value = j2m.to_jira(target.value)
-  })
+  mdControlBar.innerHTML = `
+    <input type="checkbox">
+    <label>Markdown</label>
+  `;
 
-  wikiTextArea.addEventListener("input", (e) => {
-    const target = e.target as HTMLTextAreaElement
-    mdTextArea.value = j2m.to_markdown(target.value)
-  })
-
-  // Create button
-  const mdButtonItem = document.createElement('li');
-
-  mdButtonItem.innerHTML = `<a href="#">Markdown</a>`;
-
-  mdButtonItem.addEventListener('click', () => {
-    buttonBar
-      .getElementsByClassName('aui-nav-selected')[0]
-      .classList.remove('aui-nav-selected');
-    mdButtonItem.classList.add('aui-nav-selected');
+  // TODO: Make label clickable as well
+  mdControlBar.children[0].addEventListener('click', () => {
+    toggleVisibility(wikiTextArea);
+    toggleVisibility(mdTextArea);
   });
 
-  editField.prepend(mdTextArea)
-  buttonBar.appendChild(mdButtonItem);
+  return mdControlBar;
+};
+
+const injectControl = () => {
+  const editField = document.getElementById('description-wiki-edit');
+  // const buttonBar = editField.getElementsByClassName('aui-nav')[0];
+  const wikiTextArea = document.getElementById(
+    'description'
+  ) as HTMLTextAreaElement;
+
+  // Create text area
+  const mdTextArea = document.createElement('textarea')
+  mdTextArea.style.cssText = wikiTextArea.style.cssText
+  mdTextArea.id = '';
+  
+  mdTextArea.classList.remove(
+    'wiki-editor-initialised',
+    'wiki-edit-wrapped',
+    'wiki-textfield'
+  );
+
+  toggleVisibility(mdTextArea);
+
+  mdTextArea.addEventListener('input', (e) => {
+    const target = e.target as HTMLTextAreaElement;
+    wikiTextArea.value = j2m.to_jira(target.value);
+  });
+
+  wikiTextArea.addEventListener('input', (e) => {
+    const target = e.target as HTMLTextAreaElement;
+    mdTextArea.value = j2m.to_markdown(target.value);
+  });
+
+  // Create button
+  // const mdButtonItem = document.createElement('li');
+
+  // mdButtonItem.innerHTML = `<a href="#">Markdown</a>`;
+
+  // mdButtonItem.addEventListener('click', () => {
+  //   buttonBar
+  //     .getElementsByClassName('aui-nav-selected')[0]
+  //     .classList.remove('aui-nav-selected');
+  //   mdButtonItem.classList.add('aui-nav-selected');
+  // });
+
+  editField.prepend(mdTextArea);
+  editField.prepend(createControlBar(wikiTextArea, mdTextArea));
+
+  // buttonBar.appendChild(mdButtonItem);
   return true;
 };
 
